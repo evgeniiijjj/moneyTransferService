ad5fd5e07bdc9f1ff52875aef14a67d7dd8fa51d
evgeniiijjj <100117060+evgeniiijjj@users.noreply.github.com>
2022-10-17 11:27:39 +0300

    Update ConfirmTransfer.java

--- backend/src/main/java/com/example/backend/dto/ConfirmTransfer.java
+++ backend/src/main/java/com/example/backend/dto/ConfirmTransfer.java
@@ -2,14 +2,16 @@ package com.example.backend.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
-@Getter
+@NoArgsConstructor
 public class ConfirmTransfer {
+    @Getter
     private final String operationId;
     private final String code;
 
-    public int getVerificationCode() {
+    public int getCode() {
         return Integer.parseInt(code);
     }
 }
